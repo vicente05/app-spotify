e79fac2e329154f2c1f0c73e691175614ecfa2b1
Vicente Marti Soriano <vicentemline@gmail.com>
2023-11-03 13:01:01 +0100

    prueba456

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function App() {
     return (
         <div>
-            <h1>Hola mundoooo</h1>
+            <h1>Hola mundoooooooooo</h1>
         </div>
     );
 }
